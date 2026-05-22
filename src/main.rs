758ab59b7149a4c4ee7a4a13369aaff90ba92ec8
Jack Devillier <jackdevillier@vt.edu>
2026-05-22 12:12:15 -0400

    add date widget

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use eframe::egui; // Import necessary parts of eframe and egui
+use egui_extras;
+// use chrono;
+use jiff::civil::Date;
 
 // The main function where our program starts
 fn main() -> Result<(), eframe::Error> {
@@ -15,23 +18,23 @@ fn main() -> Result<(), eframe::Error> {
 struct MyApp {
     label: String,
     value: f32,
+    date: Date,
 }
 
 // We implement the `eframe::App` trait for our struct.
 impl eframe::App for MyApp {
     // The `update` function is called repeatedly, once per frame.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-            ui.label(format!("Hello '{}', value: {}", self.label, self.value));
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        ui.heading("My egui Application");
+        ui.horizontal(|ui| {
+            ui.label("Write something: ");
+            ui.text_edit_singleline(&mut self.label);
         });
+        ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+        if ui.button("Increment").clicked() {
+            self.value += 1.0;
+        }
+        ui.label(format!("Hello '{}', value: {}", self.label, self.value));
+        ui.add(egui_extras::DatePickerButton::new(&mut self.date));
     }
 }
