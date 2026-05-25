56207e2c1f4dbbd9c665689728cd405857eccb30
Jack Devillier <jackdevillier@vt.edu>
2026-05-24 21:54:47 -0400

    song list implemented, add chords

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::any::Any;
-
 use eframe::egui; // Import necessary parts of eframe and egui
 use egui_extras;
 use chrono::{self, Datelike};
@@ -17,7 +15,7 @@ fn main() -> Result<(), eframe::Error> {
 }
 
 struct Song {
-    id: u32,
+    id: u64,
     artist: String,
     song: String
 }
@@ -32,13 +30,38 @@ struct MyApp {
     time_played_minutes: u8,
     time_played_hours: u8,
     songs: Vec<Song>,
+    next_id: u64,
+    chords: String,
+}
+
+impl MyApp {
+    fn add_song(&mut self, id: u64) {
+        self.songs.push(Song {
+            id: id,
+            artist: "".to_string(),
+            song: "".to_string()
+        });
+        self.next_id += 1;
+    }
+
+    fn remove_song(&mut self, id: u64) {
+        self.songs.retain(|s| s.id != id);
+    }
+
+    // get the current date
+    fn get_date(&mut self) -> Date {
+        let chrono_date = chrono::Local::now();
+        let ret = Date::new(chrono_date.year().try_into().unwrap(), chrono_date.month().try_into().unwrap(), chrono_date.day().try_into().unwrap());
+        return ret.unwrap();
+    }
+
 }
 
 // We implement the `eframe::App` trait for our struct.
 impl eframe::App for MyApp {
     // The `update` function is called repeatedly, once per frame.
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        ui.heading("My egui Application");
+        ui.heading("Guitar Journal");
         ui.horizontal(|ui| {
             ui.label("Write something: ");
             ui.text_edit_singleline(&mut self.label);
@@ -48,8 +71,12 @@ impl eframe::App for MyApp {
             self.value += 1.0;
         }
 
+        let mut add_clicked = false;
+        let mut id_to_remove = None;
+
         // FORM FOR NEW JOURNAL ENTRIES
         egui::Window::new("New Entry").open(&mut self.window_open).show(ui.ctx(), |ui| {
+            // ui.heading("New Entry");
             // date
             ui.horizontal(|ui| {
                 ui.label("Date of Entry: ");
@@ -70,28 +97,32 @@ impl eframe::App for MyApp {
             
             // songs
             ui.label("Song(s) Played: ");
-            let mut counter = 0;
-            ui.horizontal(|ui| {
-                // artist entry
 
-                // song entry
-
-                // remove button
-                if ui.button("-").clicked() {
-
-                }
-            });
-            
-            ui.horizontal(|ui| {
-                if ui.button("Add").clicked() {
-
-                    let _ = self.songs.push_mut(Song {id: counter, artist: "".to_string(), song: "".to_string()});
-                }
-            });
-
+            for song in &mut self.songs {
+                ui.horizontal(|ui| {
+                    // artist entry
+                    ui.label("Artist: ");
+                    ui.text_edit_singleline(&mut song.artist);
+                    
+                    // song entry
+                    ui.label("Song: ");
+                    ui.text_edit_singleline(&mut song.song);
+    
+                    // remove button
+                    if ui.button("-").clicked() {
+                        id_to_remove = Some(song.id);
+                    }
+                });
+            }
+            if ui.button("Add Song").clicked() {
+                add_clicked = true;
+            }
 
             // chords
-
+            ui.horizontal(|ui| {
+                ui.label("Chords Used: ");
+                ui.text_edit_singleline(&mut self.chords)
+            });
 
             // guitar techniques: arp, plucking, hammer on
 
@@ -99,18 +130,25 @@ impl eframe::App for MyApp {
             // new ideas for laters: scale shapes, triads, tuning
 
         });
+
+        if add_clicked {
+            self.add_song(self.next_id);
+        }
+
+        if let Some(id) = id_to_remove {
+            self.remove_song(id);
+        }
+        
         if ui.button("New Entry").clicked() {
-            self.date = get_date();
+            self.date = self.get_date();
             self.time_played_minutes = 0;
             self.time_played_hours = 0;
             self.window_open = true;
+            self.songs.clear();
+            self.chords = "".to_string();
         }
         ui.label(format!("Hello '{}', value: {}", self.label, self.value));
     }
+
 }
 
-fn get_date() -> Date {
-    let chrono_date = chrono::Local::now();
-    let ret = Date::new(chrono_date.year().try_into().unwrap(), chrono_date.month().try_into().unwrap(), chrono_date.day().try_into().unwrap());
-    return ret.unwrap();
-}
